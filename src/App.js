5b190fd5bf7b854a472cbab7ec840512af32cfc0
saife237 <saifedev@gmail.com>
2022-05-10 16:57:12 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Card from "./Components/card";
 function App() {
   return (
     <div className="App">
-     <h2>Puzzle-game In React</h2>
-      <Card/>
+
+    <Card/>
+    <h2>Puzzle-game In React</h2>
     </div>
   );
 }
